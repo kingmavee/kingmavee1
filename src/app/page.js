b5de84dbc9b68cf4d1d78e21275550e1aa4e5e26
marvelous alaje agbala <agbalaalaje@icloud.com>
2025-10-24 07:23:30 -0700

    first

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
-import LandingPage from "@/Component/Landing";
+import LandingPage from "@/component/Landing";
+
 
 
 
@@ -6,7 +7,7 @@ export default function Home() {
   return (
     <>
     
-        <LandingPage />
+        <LandingPage/>
     </>
   );
 }
